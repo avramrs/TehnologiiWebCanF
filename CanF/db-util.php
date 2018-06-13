19247ff731d9067e88468f30adca21418ef009b6
Avram Marius Alexandru <mrsavra@gmail.com>
2018-06-13 22:29:01 +0300

    Added database update function
    Not tested

--- CanF/db-util.php
+++ CanF/db-util.php
@@ -25,6 +25,15 @@ class Db
         $statement->bind_param("isissssssssss", $uploadInfo["user_id"],$uploadInfo["upload_date"], $product["cans_number"], $product["url_image"], $product["name"],$product["ingredients"],$product["packaging"], $product["quantity"],$product["serving"],$product["brand"],$product["shop"],$product["country"],$product["made_in"]);
         $statement->execute();
     }
+    public function updateProductByID($productInfo){
+        $statement = $this->conn->prepare("UPDATE products SET upload_date=?, cans_number=?, url_image=?, name=?, ingredients=?, packaging=?, quantity=?, serving=?, brand=?, shop=?, country=?, made_in=?  WHERE product_id=?");
+        $statement->bind_param("sissssssssssi", $productInfo["upload_date"], $productInfo["cans_number"], $productInfo["url_image"], $productInfo["name"],$productInfo["ingredients"],$productInfo["packaging"], $productInfo["quantity"],$productInfo["serving"],$productInfo["brand"],$productInfo["shop"],$productInfo["country"],$productInfo["made_in"],$productInfo["product_id"]);
+        $statement->execute();
+    }
+    public function getProduct($id){
+        $result = $this->conn->query("SELECT * FROM products WHERE product_id=".$id);
+        return $result->fetch_row();
+    }
 
     public function __destruct()
     {
