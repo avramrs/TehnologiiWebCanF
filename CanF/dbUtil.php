3767769a479d9b36cc692432dd45427a5b2d2b0b
Avram Marius Alexandru <mrsavra@gmail.com>
2018-06-13 12:30:16 +0300

    Database interaction class

---
+++ CanF/dbUtil.php
@@ -0,0 +1,38 @@
+<?php
+
+class Db
+{
+    var $conn = null;
+    public function __construct()
+    {
+        $this->connect();
+    }
+    private function connect()
+    {
+        if ($this->conn === null) {
+            $this->conn = new mysqli(
+                'localhost',
+                'root',
+                '',
+                'TWProject'
+            );
+        }
+    }
+
+    public function updateProduct($product, $uploadInfo)
+    {
+        $statement = $this->conn->prepare("INSERT INTO products (user_id, upload_date, cans_number, url_image, name, ingredients, packaging, quantity, serving, brand, shop, country, made_in) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $statement->bind_param("isissssssssss", $uploadInfo["user_id"],$uploadInfo["upload_date"], $product["cans_number"], $product["url_image"], $product["name"],$product["ingredients"],$product["packaging"], $product["quantity"],$product["serving"],$product["brand"],$product["shop"],$product["country"],$product["made_in"]);
+        $statement->execute();
+    }
+    public function findUser($username){
+        $res = $this->conn->query("SELECT id FROM users");
+        return $res->fetch_row()[0];
+    }
+
+    public function __destruct()
+    {
+        mysqli_close($this->conn);
+    }
+}
+?>
