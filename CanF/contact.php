833bc43ef46eefaeb2a0c8bd0541495cb07f4541
Virga Cosmin <virgacosmin@gmail.com>
2018-06-14 16:00:35 +0300

    Contact first

--- CanF/contact.php
+++ CanF/contact.php
@@ -1,1 +1,24 @@
-If you have any complaints or want to import data from other places please send us an email
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CanF</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body class="CF-no-margin">
+
+		<div id="CF-page">
+		    <div id="CF-Contact">  
+            If you have any complaints or want to import data from other places please send us an email
+            </div>  
+        </div>
+
+	<div id="CF-title-section" >
+		<div class="CF-container">
+
+		<img src="img/LogoTW2.png" alt="Logo">
+
+		</div>
+	</div>
+
+</body>
+</html>
